ea5ff4a397f6557fa5f1bf8f6be18d82c1b629fb
yingliufengpeng <2906631584@qq.com>
2025-05-11 20:29:48 +0800

    add call_go override

--- python_bind2/pybind_python.cpp
+++ python_bind2/pybind_python.cpp
@@ -3,53 +3,61 @@
 
 namespace py = pybind11;
 
-
 class Animal {
 public:
-    virtual ~Animal() { }
     virtual std::string go(int n_times) = 0;
+    virtual std::string name() { return "unknown"; }
 };
-
 class Dog : public Animal {
 public:
     std::string go(int n_times) override {
         std::string result;
         for (int i=0; i<n_times; ++i)
-            result += "woof! ";
+            result += bark() + " ";
         return result;
     }
+    virtual std::string bark() { return "woof!"; }
 };
+
+
+
+class PyAnimal : public Animal {
+public:
+    using Animal::Animal; // Inherit constructors
+    std::string go(int n_times) override { PYBIND11_OVERRIDE_PURE(std::string, Animal, go, n_times); }
+    std::string name() override { PYBIND11_OVERRIDE(std::string, Animal, name, ); }
+};
+class PyDog : public Dog {
+public:
+    using Dog::Dog; // Inherit constructors
+    std::string go(int n_times) override { PYBIND11_OVERRIDE(std::string, Dog, go, n_times); }
+    std::string name() override { PYBIND11_OVERRIDE(std::string, Dog, name, ); }
+    std::string bark() override { PYBIND11_OVERRIDE(std::string, Dog, bark, ); }
+};
+
 
 std::string call_go(Animal *animal) {
     return animal->go(3);
 }
 
-class PyAnimal : public Animal {
-public:
-    /* Inherit the constructors */
-    using Animal::Animal;
-
-    /* Trampoline (need one for each virtual function) */
-    std::string go(int n_times) override {
-        PYBIND11_OVERRIDE_PURE(
-            std::string, /* Return type */
-            Animal,      /* Parent class */
-            go,          /* Name of function in C++ (must match Python name) */
-            n_times      /* Argument(s) */
-        );
-    }
-};
+std::string call_go(Dog *animal) {
+    return animal->go(3);
+}
 
 PYBIND11_MODULE(basic, m) {
     py::class_<Animal, PyAnimal /* <--- trampoline*/>(m, "Animal")
         .def(py::init<>())
         .def("go", &Animal::go);
 
-    py::class_<Dog, Animal>(m, "Dog")
-        .def(py::init<>());
+    py::class_<Dog, PyDog>(m, "Dog")
+        .def(py::init<>())
+        .def("go", &Animal::go);
 
-    m.def("call_go", &call_go);
+
+    m.def("call_go", py::overload_cast<Animal *>(&call_go));
+    m.def("call_go", py::overload_cast<Dog *>(&call_go));
 }
+
 
 
 
--- python_bind2/python_basic_test.py
+++ python_bind2/python_basic_test.py
@@ -1,7 +1,9 @@
 import unittest
 
+
 import basic
 import tools
+from basic import Dog
 
 class TestBasic2(unittest.TestCase):
 
@@ -15,19 +17,34 @@ class TestBasic2(unittest.TestCase):
 
         print(f'test ab ...')
 
-    # def test_call_go(self):
-    #
-    #     d = basic.Dog()
-    #
-    #     v = basic.call_go(d)
-    #
-    #     class Cat(basic.Animal):
-    #         def call_go(self, ntimes):
-    #             return 'miao' * ntimes
-    #
-    #
-    #     v2 = basic.call_go(Cat())
-    #     print(f'call value is {v}')
+    def test_call_go(self):
+
+        d = basic.Dog()
+
+        v = basic.call_go(d)
+        print(f'v1 call value is {v}')
+
+        class Cat(basic.Animal):
+            def go(self, ntimes):
+                return 'miao' * ntimes
+
+
+        v2 = basic.call_go(Cat())
+        print(f'v2 call value is {v2}')
+
+
+        class Dachshund(Dog):
+            def __init__(self):
+                Dog.__init__(self)
+
+            def go(self, ntimes):
+                v = super().go(ntimes)
+                return f'{v} {ntimes} ntimes'
+
+
+        v3 = basic.call_go(Dachshund())
+        print(f'v3 : call value is {v3}')
+
 
 
 if __name__ == '__main__':
