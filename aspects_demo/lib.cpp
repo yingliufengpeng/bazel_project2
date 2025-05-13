8785e9f5cdac2c47d133f5f6cecc99fc253e3ada
yingliufengpeng <2906631584@qq.com>
2025-05-14 01:27:52 +0800

    add aspect usage using file_count_aspect rule to get *.c files count

---
+++ aspects_demo/lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peng on 5/14/25.
+//
+
+#include "lib.h"
---
+++ aspects_demo/lib.h
@@ -0,0 +1,16 @@
+//
+// Created by peng on 5/14/25.
+//
+
+#ifndef LIB_H
+#define LIB_H
+
+
+
+struct lib {
+
+};
+
+
+
+#endif //LIB_H
