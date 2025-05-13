07e204d868f3c408938620b75b491337d8dee30c
yingliufengpeng <2906631584@qq.com>
2025-05-13 14:12:20 +0800

    add lds usage

---
+++ cpp_demo_with_link_option/main.c
@@ -0,0 +1,13 @@
+//
+// Created by peng on 5/13/25.
+//
+
+
+#include <stdio.h>
+
+int global_var = 43332;
+
+int main() {
+    printf("Hello, Bazel! Global var: %d\n", global_var);
+    return 0;
+}
