b1e341d82fbe69d318c2bd0e810c7a08ec2e2213
peng <fsfsfdf@gmail.com>
2025-05-15 13:05:52 +0800

    add java usage add external project usage 2

--- java_demo/src/main/java/com/example/ProjectRunner.java
+++ java_demo/src/main/java/com/example/ProjectRunner.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.panio.Panio;
+
 public class ProjectRunner {
     public static void main(String args[]) {
         Greeting.sayHi();
+        var o = new Panio();
+        o.play();
+
     }
 }
---
+++ third_party/my-panio/src/main/java/com/panio/Panio.java
@@ -0,0 +1,11 @@
+
+package com.panio;
+
+public class Panio {
+
+    public void play() {
+         System.out.println("Hi In my Panio!");
+
+    }
+
+}
