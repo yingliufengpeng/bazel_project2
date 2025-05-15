9e54e402d97ee013e403646e6f15dacc000444cc
peng <yingliufengpeng@gmail.com>
2025-05-15 09:46:36 +0800

    add third party usage

---
+++ third_party/my_lib/include/my_header.h
@@ -0,0 +1,12 @@
+//
+// Created by peng on 15/5/2025.
+//
+
+#ifndef BAZEL_PROJECT1_MY_HEADER_H
+#define BAZEL_PROJECT1_MY_HEADER_H
+
+int add(int a, int  b) {
+    return a + b;
+}
+
+#endif //BAZEL_PROJECT1_MY_HEADER_H
