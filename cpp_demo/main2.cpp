092f86c1eaacb74e5e80e55c026c39e2ce80857b
peng <fsfsfdf@gmail.com>
2025-05-15 09:46:36 +0800

    add third party usage

---
+++ cpp_demo/main2.cpp
@@ -0,0 +1,11 @@
+//
+// Created by peng on 15/5/2025.
+//
+#include "third_party/my_lib/my_header.h"
+
+#include <iostream>
+int main() {
+    std::cout << "3 + 4 = "  << add(3, 4) << std::endl;
+    add(3, 4);
+    return 0;
+}
