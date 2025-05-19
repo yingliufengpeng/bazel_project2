5c52629d72e51cf1db439dfcd11656104de083d1
yingliufengpeng <2906631584@qq.com>
2025-05-19 13:42:00 +0800

    add generate cpp with template file code usage.

--- generate_cpp_demo/generate_cpp.py
+++ generate_cpp_demo/generate_cpp.py
@@ -23,21 +23,23 @@ namespace generated {{
 '''
 
 
-def generate_cpp_code(output_path, class_name, message):
-    with open(output_path, 'w') as f:
-        f.write(cpp_code.format(class_name=class_name, message=message))
+def generate_cpp_code(output_path, tmplate_file, class_name, message):
+    with open(tmplate_file) as f_cpp_template:
+        cpp_code = f_cpp_template.read()
+        with open(output_path, 'w') as f:
+            f.write(cpp_code.format(class_name=class_name, message=message))
 
 
 if __name__ == '__main__':
     import sys
     print(f'xxxxx {sys.argv}')
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("Usage: generate_cpp.py usage <output> <class_name>  <message> ")
         sys.exit(-1)
 
-    _, output, class_name, message = sys.argv
+    _, output, tmplate_file, class_name, message = sys.argv
 
     print(f'----------------- {output} -----')
 
 
-    generate_cpp_code(output, class_name, message)
+    generate_cpp_code(output,tmplate_file, class_name, message)
