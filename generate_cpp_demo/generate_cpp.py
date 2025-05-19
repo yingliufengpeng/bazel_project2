f50739523d71b265dba7787d094456d627af4e95
peng <yingliufengpeng@gmail.com>
2025-05-19 13:29:25 +0800

    add genrule for windows cmd_bat usage

--- generate_cpp_demo/generate_cpp.py
+++ generate_cpp_demo/generate_cpp.py
@@ -30,8 +30,10 @@ def generate_cpp_code(output_path, class_name, message):
 
 if __name__ == '__main__':
     import sys
+    print(f'xxxxx {sys.argv}')
     if len(sys.argv) != 4:
         print("Usage: generate_cpp.py usage <output> <class_name>  <message> ")
+        sys.exit(-1)
 
     _, output, class_name, message = sys.argv
 
