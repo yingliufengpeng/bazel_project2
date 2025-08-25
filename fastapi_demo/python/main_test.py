b2d5f973d4b0354d353d4b90f1d22256de43c8c8
peng <fsfsfdf@gmail.com>
2025-08-26 00:14:57 +0800

    update

--- fastapi_demo/python/main_test.py
+++ fastapi_demo/python/main_test.py
@@ -6,6 +6,7 @@ class TestBasic(unittest.TestCase):
 
     def test_add(self):
         import os
+        import fastapi
         from fastapi import FastAPI
         # import gast
         print(f'xxxx {fastapi.__file__}')
