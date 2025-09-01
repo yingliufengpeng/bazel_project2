e9d8096d883fc14a7141e969c01262038b1a4762
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-01 04:35:25 +0000

    linux /third_party/gdal:main run/debug success2

--- third_party/gdal/main.cpp
+++ third_party/gdal/main.cpp
@@ -25,9 +25,12 @@ using rules_cc::cc::runfiles::Runfiles;
 int main(int argc, char *argv[]) {
     std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
     std::cout << "input file :" << argv[1] << std::endl;
-     std::unique_ptr<Runfiles> runfiles(Runfiles::Create(argv[0]));(argv[1]);
+     std::unique_ptr<Runfiles> runfiles(Runfiles::Create(argv[0]));
 //     std::string real_path = runfiles->Rlocation(argv[1]);
-     std::string real_path = runfiles->Rlocation("bazel_project2/thrid_party/gdal/WI_Poynette_801263_1901_62500_geo.tif");
+    const std::string workspace_prefix = "_main/";
+
+     // std::string real_path = workspace_prefix + runfiles->Rlocation("third_party/gdal/WI_Poynette_801263_1901_62500_geo.tif");
+     std::string real_path = runfiles->Rlocation( workspace_prefix + argv[1]);
      auto file_name = real_path.c_str();
 //    auto file_name = argv[1];
     std::cout << "arc 22 " << argc << " 传递进来的文件为: " << file_name << std::endl;
