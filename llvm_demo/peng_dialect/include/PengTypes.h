2ac0b2f9daf8f81320bd6cc46550a352c88056e6
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-07 16:27:09 +0000

    PengDialect  build success

--- llvm_demo/peng_dialect/include/PengTypes.h
+++ llvm_demo/peng_dialect/include/PengTypes.h
@@ -5,6 +5,7 @@
 #ifndef BAZEL_PROJECT2_PENGTYPES_H2
 #define BAZEL_PROJECT2_PENGTYPES_H2
 #include <iostream>
+
 #include "mlir/include/mlir/IR/Types.h"
 #define GET_TYPEDEF_CLASSES
 #include "llvm_demo/peng_dialect/PengTypes.h.inc"
--- llvm_demo/peng_dialect/main.cpp
+++ llvm_demo/peng_dialect/main.cpp
@@ -18,9 +18,11 @@ int main() {
     diaglect->sayHello();
     auto f32 = mlir::Float32Type::get(&context);
     llvm::outs() << f32;
+    llvm::outs() << "\n";
     auto peng_tensor = mlir::peng::PTensorType::get(&context, {3, 4}, mlir::Float64Type::get(&context), 3);
     llvm::outs() << "peng_tensor" << "\t";
     peng_tensor.dump();
+    llvm::outs() << "\n";
 
     return 0;
 }
