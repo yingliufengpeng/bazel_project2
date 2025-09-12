88733f4fdcdc821abd34e9fb6baead66917f2749
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-12 16:04:12 +0000

    add template auto syntax usage

--- cpp_demo/main2.cpp
+++ cpp_demo/main2.cpp
@@ -4,8 +4,19 @@
 #include "third_party/my_lib/my_header.h"
 
 #include <iostream>
+
+template <typename X, typename X2, typename Y>
+auto fn(Y&& y) {
+    return y;
+
+}
+
 int main() {
     std::cout << "3 + 4 = "  << add(3, 4) << std::endl;
     add(3, 4);
+
+    auto b = fn<std::string, int>(43);
+    std::cout << b << std::endl;
+
     return 0;
 }
