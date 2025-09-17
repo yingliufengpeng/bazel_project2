10c916aafeccb5df9d822f4dff1be8c420c32381
yingliufengpeng <2906631584@qq.com>
2025-09-17 07:34:48 +0000

    add rust with cargo support only with linux

--- rust_demo/main/main.cc
+++ rust_demo/main/main.cc
@@ -3,8 +3,13 @@
 extern "C" int random_add(int);
 extern "C" void make_person();
 
+void foo() {
+    std::cout << "Hello world!" << std::endl;
+}
+
 int main() {
     std::cout << "3 + random = " << random_add(3) << std::endl;
     make_person();
+    foo();
     return 0;
 }
