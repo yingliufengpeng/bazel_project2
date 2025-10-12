ba2183b58cddbebb87433b364d282b261dfe4cc3
yingliufengpeng <2906631584@qq.com>
2025-10-12 03:17:01 +0000

    add cpython sub module support with cpp

--- python_bind5/main.cc
+++ python_bind5/main.cc
@@ -1,8 +1,46 @@
 #include <Python.h>
 #include <iostream>
 
-#include "lib/Stu.h"
+// #include "lib/Stu.h"
 #include "lib/demo_type.h"
+
+static PyObject* sub_hello(PyObject* self, PyObject* args) {
+    // 打印类型名
+    PySys_WriteStdout("self type: %s\n", Py_TYPE(self)->tp_name);
+
+    // 打印 repr(self)
+    PyObject* repr = PyObject_Repr(self);
+    if (repr) {
+        PySys_WriteStdout("self = %s\n", PyUnicode_AsUTF8(repr));
+        Py_DECREF(repr);
+    } else {
+        PySys_WriteStderr("Failed to get repr(self)\n");
+        PyErr_Clear();  // 防止异常干扰
+    }
+
+    // 如果是模块，打印模块名
+    if (PyModule_Check(self)) {
+        PyObject* name = PyObject_GetAttrString(self, "__name__");
+        if (name) {
+            PySys_WriteStdout("module name = %s\n", PyUnicode_AsUTF8(name));
+            Py_DECREF(name);
+        }
+    }
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef SubMethods[] = {
+    {"hello", sub_hello, METH_NOARGS, "Print hello from C++ submodule"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef submodule_def = {
+    PyModuleDef_HEAD_INIT,
+    "demo.submodule",  // 全限定名
+    "C++ implemented submodule",
+    -1,
+    SubMethods,
+};
 
 int main(int argc, char** argv) {
     Py_Initialize();
@@ -11,18 +49,39 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    if (!initModule()) {
-        std::cerr << "Failed to initModule" << std::endl;
+    PyObject* m;
+
+
+    m = PyModule_Create(&submodule_def);
+    if (m == NULL)
+        return -1;
+
+    // 2. 创建父模块 demo
+    PyObject* demo_mod = PyModule_New("demo");
+    if (!demo_mod) {
+        std::cerr << "Failed to create parent module" << std::endl;
+        return 1;
+    }
+    // 4. 把父模块注册到 sys.modules
+    PyObject* sys_modules = PyImport_GetModuleDict();
+    PyDict_SetItemString(sys_modules, "demo", demo_mod);
+
+    PyObject* sub_m;
+    if (!((sub_m = PyInit_demo(demo_mod)))) {
+        PySys_WriteStderr("Failed to get PyInit_demo\n");
+
     }
 
-    if (!PyInit_demo()) {
-        std::cerr << "Failed to PyInit_demo" << std::endl;
-    }
+
+    PyDict_SetItemString(sys_modules, "demo.sub_m", sub_m);
+
 
     std::cout << "Python initialized" << std::endl;
     PyRun_SimpleString("print('Hello from embedded Python with Bazel module mode!')");
     PyRun_SimpleString("import sys; print('Python version:', sys.version)");
     PyRun_SimpleString("import os; print('os __file__:', os.__file__)");
+    PyRun_SimpleString("import demo; print('demo __name__:', demo.__name__)");
+    PyRun_SimpleString("import demo.sub_m; print('demo.sub_m __name__:', demo.sub_m.__name__)");
     Py_Finalize();
     return 0;
 }
