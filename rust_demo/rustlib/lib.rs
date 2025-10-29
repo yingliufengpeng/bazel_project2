0cd2eabd050c166f0f0e8ce8f6ae4d977820a9e5
peng <2906631584@qq.com>
2025-10-29 14:23:37 +0800

    add Student rust struct

--- rust_demo/rustlib/lib.rs
+++ rust_demo/rustlib/lib.rs
@@ -7,6 +7,34 @@ pub struct Person {
     age: i32,
 }
 
+#[derive(Debug)]
+struct Student {
+    name: String,
+    age: i32,
+    vec: Vec<String>,
+}
+
+fn main() {
+    let p = Person {
+        name: "Alice".to_string(),
+        age: 30,
+        vec: vec!["C++".to_string(), "Rust".to_string()],
+    };
+
+    println!("{:?}", p);
+}
+
+impl Student {
+    pub fn new(name: String, age: i32) -> Self {
+        Person { name, age, vec: vec![] }
+    }
+
+    pub fn add(&mut self, message: String) {
+        self.vec.push(message);
+    }
+
+}
+
 #[no_mangle]
 pub extern "C" fn make_person() -> Person {
     let name = CString::new("Alice").unwrap();
@@ -14,6 +42,11 @@ pub extern "C" fn make_person() -> Person {
         name: name.into_raw(),  // 将 CString 转为裸指针
         age: 30,
     };
+
+    let mut m = Student::new(name.into_raw(), 30);
+    m.add("C++".to_string());
+    m.add("Python");
+    println!("Student: {#?}", m);
     p
 }
 #[no_mangle]
