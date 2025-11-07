5baae274634ff9ba618027f0a756d9bd1a069d90
peng <2906631584@qq.com>
2025-11-07 17:30:17 +0800

    c_demo fixup: computation logic

--- c_demo/main.c
+++ c_demo/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 /*
-  兼容 Windows 的宏化循环式虚拟机
-  模拟逻辑：10 + 20
+  兼容 Windows/Linux 的宏化循环式虚拟机
+  模拟逻辑：10 + 20 - 34
 */
 
 enum {
     OP_PUSH_CONST,
     OP_ADD,
+    OP_SUB,
     OP_PRINT_TOP,
     OP_HALT,
 };
@@ -16,29 +17,29 @@ enum {
 #define PUSH_STACK()  stack[sp++] = (arg)
 #define STACK_SET_TOP(x) stack[sp - 1] = (x)
 #define STACK_TOP() stack[sp - 1]
-#define STACK_VALUE(offset) stack[sp - offset]
+#define STACK_VALUE(offset) stack[sp + (offset)]
 #define STACK_SP_ADD() sp += 1
 #define STACK_SP_SUB() sp -= 1
-#define NEXTOPCODE() (opcode = bytecode[pc++])
-#define NEXTARG() (arg = bytecode[pc++])
+#define NEXT_OP_CODE()  opcode = bytecode[pc++]
+#define NEXT_ARG() (arg = bytecode[pc++])
 #define DISPATCH_EXIT() goto end  /* switch-case 内用 break 跳到下次循环 */
 
 #if defined(_WIN32)
 
     #define TARGET(op) case op
 
-    #define DISPATCH() break  /* switch-case 内用 break 跳到下次循环 */
+    #define DISPATCH() \
+        NEXT_OP_CODE(); \
+        break;  /* switch-case 内用 break 跳到下次循环 */
     #define DEFATULT default
 
 #else
 
-
 #define TARGET(op) op
 #define DISPATCH() \
-    NEXTOPCODE(); \
-    goto *opcode_targets[opcode];  /* switch-case 内用 break 跳到下次循环 */
+    NEXT_OP_CODE(); \
+    goto *opcode_targets[opcode];  /* switch-case 内用 compute_gotos 跳到下次地址 */
 #define DEFATULT default_end
-
 
 
 #endif
@@ -46,22 +47,25 @@ enum {
 
 
 int main( ) {
-
 #if defined(_WIN32)
 #else
     static void *opcode_targets[] = {
         &&TARGET(OP_PUSH_CONST),
         &&TARGET(OP_ADD),
+        &&TARGET(OP_SUB),
         &&TARGET(OP_PRINT_TOP),
         &&TARGET(OP_HALT),
     };
 #endif
 
     int bytecode[] = {
-        OP_PUSH_CONST, 10,
+        OP_PUSH_CONST, 11,
         OP_PUSH_CONST, 20,
         OP_ADD,
-        OP_PUSH_CONST, 34,
+        OP_PUSH_CONST, 31,
+        OP_SUB,
+        OP_PUSH_CONST, 200,
+        OP_ADD,
         OP_PRINT_TOP,
         OP_HALT
     };
@@ -75,44 +79,46 @@ int main( ) {
     goto start_frame;
 #if defined(_WIN32)
 
-    for (;; ) {
+    for (;;) {
+        switch (opcode) {
 #else
     {
 #endif
 
         TARGET(OP_PUSH_CONST): {
-            NEXTARG();
+            NEXT_ARG();
             PUSH_STACK();
-
             DISPATCH()
-
         }
 
         TARGET(OP_ADD): {
-
-            int r1 = STACK_VALUE(2) + STACK_VALUE(1);
-            STACK_SET_TOP(r1);
+            int r1 = STACK_VALUE(-2) + STACK_VALUE(-1);
             STACK_SP_SUB();
+
+            STACK_SET_TOP(r1);
+            DISPATCH();
+        }
+
+        TARGET(OP_SUB): {
+            int r1 = STACK_VALUE(-2)  - STACK_VALUE(-1);
+            STACK_SP_SUB();
+            STACK_SET_TOP(r1);
             DISPATCH();
         }
 
         TARGET(OP_PRINT_TOP): {
-
-            printf("%d\n", STACK_TOP());
-            STACK_SP_SUB();
+            printf("TOP Value is %d\n", STACK_TOP());
+            // STACK_SP_SUB();
             DISPATCH()
-
         }
 
         TARGET(OP_HALT): {
-
             printf("Halt the programm!!\n");
             DISPATCH_EXIT();
         }
 
         DEFATULT: {
             printf("Halt ERROR running here!!\n");
-
             DISPATCH_EXIT();
         }
 
@@ -122,11 +128,10 @@ int main( ) {
 
 #if defined(_WIN32)
     }
+}
 #else
     }
 #endif
-
-
 
     end:
     return 0;
